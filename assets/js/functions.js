ddc6ca6bb3776fb95d1e93f8d02c41c8d45e16bc
MRBrath93 <147531334+MRBrath93@users.noreply.github.com>
2024-04-29 14:28:26 +0200

    Næsten færdig

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,11 @@
 const baseUrl = "https://foodista.brathcodestudio.com/wp-json/wp/v2/";
-const freshNewsEl = document.querySelector(".freshNews")
+const freshNewsEl = document.querySelector(".freshNews");
+const freshPostsEl = document.querySelector(".freshPosts");
 let newsData;
 
 
-function getAmountOfNews(quantity) {
-    return fetch(baseUrl + `posts?per_page=` + quantity + `&categories=95`)
+function getAmountOfNews(quantity, categoryId) {
+    return fetch(baseUrl + `posts?per_page=` + quantity + `&categories=` + categoryId)
         .then((res) => res.json())
         .then((news) => {
             console.log(news)
@@ -14,7 +15,6 @@ function getAmountOfNews(quantity) {
         .catch(err => console.log("Fejl", err));
 }
 
-getAmountOfNews(3)
 
 
 function renderNews(containerToFill, news) {
@@ -22,11 +22,33 @@ function renderNews(containerToFill, news) {
     containerToFill.innerHTML = "";
     news.forEach(story => {
         containerToFill.innerHTML += `<div class="newNews">
-        <img src="${story.acf.image.sizes.medium}" alt="${story.acf.image.alt}">
-        <h3>${story.acf.title}</h3>
+        <img src="${story.acf.image.sizes.large}" alt="${story.acf.image.alt}">
+        <h3>${story.acf.titel}</h3>
         </div>
         `
     });
 }
 
-renderNews(freshNewsEl, news)
+getAmountOfNews(3, 95)
+    .then(news => renderNews(freshNewsEl, news))
+    .catch(err => console.error("Fejl under hentning af opskrifter:", err));
+
+
+
+function renderPosts(containerToFill, news) {
+
+    containerToFill.innerHTML = "";
+    news.forEach(story => {
+        containerToFill.innerHTML += `<div class="newPosts">
+            <img src="${story.acf.image_1.sizes.large}" alt="${story.acf.image_1.alt}">
+            <h3>${story.acf.title}</h3>
+            </div>
+            `
+    });
+}
+
+
+
+getAmountOfNews(3, 107)
+    .then(news => renderPosts(freshPostsEl, news))
+    .catch(err => console.error("Fejl under hentning af opskrifter:", err));
