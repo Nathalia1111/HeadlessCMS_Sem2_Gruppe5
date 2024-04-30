0d6d34813cfc6d093883502af4095a44099fbb98
MRBrath93 <147531334+MRBrath93@users.noreply.github.com>
2024-04-30 11:24:01 +0200

    Update recipes.js

---
+++ assets/js/recipes.js
@@ -0,0 +1,18 @@
+const recipeIndexEl = document.querySelector(".recipeIndex")
+const thisWeekEl = document.querySelector(".thisWeek")
+const resetFiltersBtn = document.querySelector(".resetFilter");
+
+
+getAmountOfRecipes(100)
+    .then(recipes => renderAllrecipes(recipeIndexEl, recipes))
+    .catch(err => console.error("Fejl under hentning af opskrifter:", err));
+
+
+getAmountOfRecipes(5)
+    .then(recipes => renderNewrecipes(thisWeekEl, recipes))
+    .catch(err => console.error("Fejl under hentning af opskrifter:", err));
+
+
+document.addEventListener("DOMContentLoaded", useFilter);
+
+resetFiltersBtn.addEventListener("click", restAllFilters);
